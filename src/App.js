8040df9bcd640131af9ccb173664c5190b73c14d
Sean <sean.n.cordes@gmail.com>
2020-08-29 16:54:28 -0400

    created a computer that has hardcoded moves, it will win/draw most/all times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 
+// allow changing of levels
+// level 1 — random
+// level 2 - random with a % playing best move
+// level 3 - always play best move
+
+// allow user to switch between O and X
+
+// use algorithms and not machine learning, this is a solved problem, it doesn't need AI
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -53,7 +62,6 @@ class Game extends React.Component {
       ],
       stepNumber: 0,
       xIsNext: true,
-      computerIsO: true,
     };
   }
 
@@ -61,7 +69,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares) || squares[i] || this.state.xIsNext) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -76,20 +84,18 @@ class Game extends React.Component {
     });
   }
 
-  componentDidUpdate() {
-    if (!this.state.xIsNext) {
-      let numbers = []
-      let n;
-      for (n = 0; n < 9; n++) {
-        if (!this.state.history[this.state.stepNumber].squares[n]) {
-          numbers.push(n)
-        }
-      }
+  componentDidMount() {
+    this.componentDidUpdate();
+  }
 
-      const i = randomItem(numbers)
+  componentDidUpdate() {
+    if (this.state.xIsNext) {
+      // disable all buttons
+
       const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
+      const i = computerPlay(squares);
 
       if (calculateWinner(squares) || squares[i]) {
         return;
@@ -105,10 +111,12 @@ class Game extends React.Component {
             }
           ]),
           stepNumber: history.length,
-          xIsNext: !this.state.xIsNext
+          xIsNext: false
         }), 
         1000
       );
+
+      // enable all buttons - maybe in the callback
     }
   }
 
@@ -139,7 +147,7 @@ class Game extends React.Component {
     if (winner) {
       status = "Winner: " + winner;
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "You" : "Computer");
+      status = "Next player: " + (this.state.xIsNext ? "Computer" : "You");
     }
 
     return (
@@ -172,6 +180,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -179,6 +188,78 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+function computerPlay(squares) {
+  const nullNumbers = [];
+  const oWinning = [];
+  const oNumbers = [];
+  const xNumbers = [];
+
+
+  for (let i = 0; i < 9; i++) {  
+
+    let tempSquares = squares;
+
+    if (tempSquares[i] === 'X') {
+      xNumbers.push(i)
+    } else if (tempSquares[i] === 'O') {
+      oNumbers.push(i)
+    } else {
+      // can X win?
+      tempSquares[i] = 'X'
+      if (calculateWinner(tempSquares)) {
+        tempSquares[i] = null
+        return i;
+      }
+
+      // can O win?
+      tempSquares[i] = 'O'
+      if (calculateWinner(tempSquares)) {
+        oWinning.push(i)
+      }
+
+      // keep track of nulls for random assignment
+      tempSquares[i] = null
+      nullNumbers.push(i)
+    }
+  }
+
+  // return a number to block
+  if (oWinning.length > 0) {
+    return oWinning[0];
+  }
+
+  // first move is always 6
+  if (nullNumbers.length === 9) {
+    return 6;
+  }
+
+
+  const edges = [1, 3, 5, 7]
+  const corners = [0, 2, 8]
+  const center = 4
+
+  // if O plays once
+  if (oNumbers.length === 1) {
+    // play edges
+    if (edges.includes(oNumbers[0])) {
+      return 2
+    }
+
+    // play corner
+    if (corners.includes(oNumbers[0])) {
+      return corners[1] !== oNumbers[0] ? 2 : 8
+    }
+
+    // play center
+    if (center === oNumbers[0]) {
+      return 2
+    }
+  }
+
+  // return random number
+  return randomItem(nullNumbers);
 }
 
 function randomItem(items) {
